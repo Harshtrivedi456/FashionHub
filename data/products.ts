c727f57d943b8fd2a4b5c1313dcfb3fc2fb616b7
Harsh Trivedi <152944798+Harshtrivedi456@users.noreply.github.com>
2025-01-27 21:37:20 +0530

    Create products.ts

---
+++ data/products.ts
@@ -0,0 +1,306 @@
+export const products = [
+  {
+    id: 1,
+    name: "Classic White T-Shirt",
+    price: 19.99,
+    description: "A comfortable and versatile white t-shirt made from 100% organic cotton. Perfect for everyday wear.",
+    image:
+      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    category: "Men's Wear",
+    rating: 4.5,
+    reviews: 120,
+    sizes: ["S", "M", "L", "XL"],
+    colors: ["White", "Black", "Gray"],
+  },
+  {
+    id: 2,
+    name: "Slim Fit Denim Jeans",
+    price: 49.99,
+    description:
+      "Classic blue denim jeans with a modern slim fit. Made from durable denim with a touch of stretch for comfort.",
+    image:
+      "https://images.unsplash.com/photo-1542272604-787c3835535d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    category: "Men's Wear",
+    rating: 4.2,
+    reviews: 85,
+    sizes: ["30x32", "32x32", "34x32", "36x32"],
+    colors: ["Blue", "Black", "Gray"],
+  },
+  {
+    id: 3,
+    name: "Men's Casual Trouser",
+    price: 69.99,
+    description:
+      "Men’s casual trousers: where style meets comfort, perfect for laid-back days or smart-casual vibes!",
+    image:
+      "https://images.unsplash.com/photo-1473966968600-fa801b869a1a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Men's Wear",
+    rating: 4.2,
+    reviews: 85,
+    sizes: ["30x32", "32x32", "34x32", "36x32"],
+    colors: ["Blue", "Black", "White"],
+  },
+  {
+    id: 4,
+    name: "Floral Summer Dress",
+    price: 39.99,
+    description: "A light and breezy dress with a beautiful floral print. Perfect for summer days and outdoor events.",
+    image:
+      "https://images.unsplash.com/photo-1572804013309-59a88b7e92f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    category: "Women's Wear",
+    rating: 4.7,
+    reviews: 150,
+    sizes: ["XS", "S", "M", "L"],
+    colors: ["Floral Print", "Solid Blue", "Solid Pink"],
+  },
+  {
+    id: 5,
+    name: "Leather Biker Jacket",
+    price: 89.99,
+    description:
+      "A stylish leather jacket to complete any outfit. Made from genuine leather with a comfortable polyester lining.",
+    image:
+      "https://images.unsplash.com/photo-1551028719-00167b16eac5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    category: "Men's Wear",
+    rating: 4.8,
+    reviews: 200,
+    sizes: ["S", "M", "L", "XL"],
+    colors: ["Black", "Brown"],
+  },
+  {
+    id: 6,
+    name: "Wrist Watch",
+    price: 149.99,
+    description:
+      "A stylish leather jacket to complete any outfit. Made from genuine leather with a comfortable polyester lining.",
+    image:
+      "https://images.unsplash.com/photo-1524738258074-f8125c6a7588?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bWVuJ3MlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D",
+    category: "Men's Wear",
+    rating: 4.8,
+    reviews: 200,
+    sizes: ["S", "M", "L", "XL"],
+    colors: ["Black", "Brown"],
+  },
+  {
+    id: 7,
+    name: "Striped Polo Shirt",
+    price: 29.99,
+    description: "A casual striped polo shirt for a smart look. Made from breathable cotton pique fabric.",
+    image:
+      "https://images.unsplash.com/photo-1626497764746-6dc36546b388?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    category: "Men's Wear",
+    rating: 4.3,
+    reviews: 75,
+    sizes: ["S", "M", "L", "XL", "XXL"],
+    colors: ["Blue/White Stripe", "Red/Navy Stripe", "Green/White Stripe"],
+  },
+  {
+    id: 8,
+    name: "A-Line Midi Skirt",
+    price: 34.99,
+    description:
+      "A beautiful A-line midi skirt for a feminine touch. Features a high waist and comfortable elastic band.",
+    image:
+      "https://images.unsplash.com/photo-1577900232427-18219b9166a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    category: "Women's Wear",
+    rating: 4.6,
+    reviews: 110,
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: ["Black", "Navy", "Burgundy"],
+  },
+  {
+    id: 9,
+    name: "Maxi Desses",
+    price: 34.99,
+    description:
+      "Maxi dresses where comfort meets elegance, flowing effortlessly from casual brunches to glamorous evenings!",
+    image:
+      "https://images.unsplash.com/photo-1605763240000-7e93b172d754?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1heGklMjBkcmVzc3xlbnwwfHwwfHx8MA%3D%3D",
+    category: "Women's Wear",
+    rating: 4.6,
+    reviews: 110,
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: ["Black", "Navy", "Burgundy"],
+  },
+  {
+    id: 10,
+    name: "Saree",
+    price: 156.99,
+    description:
+      "Saree: A timeless blend of tradition and grace, draped to perfection for every special occasion.",
+    image:
+      "https://images.unsplash.com/photo-1617627143750-d86bc21e42bb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2FyZWV8ZW58MHx8MHx8fDA%3D",
+    category: "Women's Wear",
+    rating: 4.6,
+    reviews: 110,
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: ["Black", "Navy", "Burgundy"],
+  },
+  {
+    id: 11,
+    name: "Hoodies",
+    price: 156.99,
+    description:
+     "Cozy up in style with hoodies that blend comfort, warmth, and effortless chic.",
+    image:
+      "https://images.unsplash.com/photo-1526476148966-98bd039463ea?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Women's Wear",
+    rating: 4.6,
+    reviews: 110,
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: ["Black", "Navy", "Burgundy"],
+  },
+  {
+    id: 12,
+    name: "Kurta",
+    price: 156.99,
+    description:
+      "Kurta: Effortless elegance, where heritage meets modern style, perfect for every occasion.",
+    image:
+      "https://images.unsplash.com/photo-1597983073493-88cd35cf93b0?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Women's Wear",
+    rating: 4.6,
+    reviews: 110,
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: ["Black", "Navy", "Burgundy"],
+  },
+  {
+    id: 13,
+    name: "Kids Dinosaur T-Shirt",
+    price: 14.99,
+    description: "A fun t-shirt with a colorful dinosaur print for kids. Made from soft, comfortable cotton.",
+    image:
+      "https://images.unsplash.com/photo-1519278409-1f56fdda7fe5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    category: "Children's Wear",
+    rating: 4.9,
+    reviews: 80,
+    sizes: ["3-4Y", "5-6Y", "7-8Y", "9-10Y"],
+    colors: ["Blue", "Green", "Red"],
+  },
+   {
+    id: 14,
+    name: "Kids Hoodies",
+    price: 14.99,
+    description:  "Snuggle up in style—hoodies that bring comfort and fun together.",
+    image:
+      "https://plus.unsplash.com/premium_photo-1698305282759-0d295cbb96d6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2hpbGRlcm4ncyUyMGhvb2RpZXN8ZW58MHx8MHx8fDA%3D",
+    category: "Children's Wear",
+    rating: 4.9,
+    reviews: 80,
+    sizes: ["3-4Y", "5-6Y", "7-8Y", "9-10Y"],
+    colors: ["Blue", "Green", "Red"],
+  },
+  {
+    id: 15,
+    name: "Rompers",
+    price: 14.99,
+    description: "Tiny comfort in every step, with rompers that are as cute as they are cozy.",
+    image:
+      "https://plus.unsplash.com/premium_photo-1693242804074-20a78966f4e6?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Children's Wear",
+    rating: 4.9,
+    reviews: 80,
+    sizes: ["3-4Y", "5-6Y", "7-8Y", "9-10Y"],
+    colors: ["Blue", "Green", "Red"],
+  },
+  {
+    id: 16,
+    name: "Baba Jacket",
+    price: 14.99,
+    description: "Stay snug and stylish—jackets that keep little adventurers warm and ready for anything.",
+    image:
+      "https://plus.unsplash.com/premium_photo-1700429926385-23bcfbadf10b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Children's Wear",
+    rating: 4.9,
+    reviews: 80,
+    sizes: ["3-4Y", "5-6Y", "7-8Y", "9-10Y"],
+    colors: ["Blue", "Green", "Red"],
+  },
+  {
+    id: 17,
+    name: "Cheques Shirts",
+    price: 14.99,
+    description: "Brighten their day with shirts that are as playful as they are!",
+    image:
+      "https://images.unsplash.com/photo-1518463231256-578cfa07d614?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Children's Wear",
+    rating: 4.9,
+    reviews: 80,
+    sizes: ["3-4Y", "5-6Y", "7-8Y", "9-10Y"],
+    colors: ["Blue", "Green", "Red"],
+  },
+  {
+    id: 18,
+    name: "Silver Chain Necklace",
+    price: 79.99,
+    description:
+      "An elegant silver chain necklace to accessorize any outfit. Made from sterling silver with a lobster clasp.",
+    image:
+      "https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    category: "Accessories",
+    rating: 4.7,
+    reviews: 95,
+    sizes: ['16"', '18"', '20"', '24"'],
+    colors: ["Silver"],
+  },
+  {
+    id: 19,
+    name: "Bangles",
+    price: 79.99,
+    description:
+      "Stack your wrist with bangle bliss—where tradition meets modern sparkle.",
+    image:
+      "https://images.unsplash.com/photo-1601482438629-346a273776af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFuZ2xlc3xlbnwwfHwwfHx8MA%3D%3D",
+    category: "Accessories",
+    rating: 4.7,
+    reviews: 95,
+    sizes: ['16"', '18"', '20"', '24"'],
+    colors: ["Silver"],
+  },
+  {
+    id: 20,
+    name: "Earings",
+    price: 79.99,
+    description:
+       "Let your ears shine with earrings that make every outfit unforgettable.",
+    image:
+      "https://images.unsplash.com/photo-1630019852942-f89202989a59?q=80&w=1946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Accessories",
+    rating: 4.7,
+    reviews: 95,
+    sizes: ['16"', '18"', '20"', '24"'],
+    colors: ["Silver"],
+  },
+    {
+    id: 21,
+    name: "Rings",
+    price: 79.99,
+    description:
+      "A perfect ring for every finger, adding a touch of elegance to your every gesture.",
+    image:
+      "https://images.unsplash.com/photo-1550368566-f9cc32d7392d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Accessories",
+    rating: 4.7,
+    reviews: 95,
+    sizes: ['16"', '18"', '20"', '24"'],
+    colors: ["Silver"],
+  },
+      {
+    id: 22,
+    name: "Braceltes",
+    price: 79.99,
+    description:
+      "Wrap your wrist in elegance with bracelets that blend style and sophistication.",
+    image:
+      "https://plus.unsplash.com/premium_photo-1680181362119-5c9bf196805f?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Accessories",
+    rating: 4.7,
+    reviews: 95,
+    sizes: ['16"', '18"', '20"', '24"'],
+    colors: ["Silver"],
+  },
+
+]
+
+export const categories = ["Men's Wear", "Women's Wear", "Children's Wear", "Accessories"]
+
